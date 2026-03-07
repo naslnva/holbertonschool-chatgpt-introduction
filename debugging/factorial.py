e75068773a1e78d125fb589a15237a183d86c011
Naila Aslanova <13585@holbertonstudents.com>
2026-03-07 23:56:38 +0400

    factorial.py

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 import sys
 
-for i in range(1, len(sys.argv)):
-    print(sys.argv[i])
+def factorial(n):
+    result = 1
+    while n > 1:
+        result *= n
+        n -= 1
+    return result
+
+f = factorial(int(sys.argv[1]))
+print(f)
